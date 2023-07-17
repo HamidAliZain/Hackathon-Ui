88e51e8f5e06fb8835b037582b3aa5c49e81bad9
HAmid <hamidprince823@gmail.com>
2023-07-17 14:52:59 +0500

    some modification

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,11 +1,15 @@
+import Promotion from "@/components/Promotion";
 import ProductCard from "@/components/productCard";
+import { About } from "@/components/view/About";
 import Hero from "@/components/view/Hero";
 
 export default function Home() {
   return (
-    <main>
+    <main className="  md:px-10">
       <Hero />
+      <Promotion />
       <ProductCard />
+      <About />
     </main>
   );
 }
